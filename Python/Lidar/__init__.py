0f08e53c7918fd8c814faa9de4f963b6773295b6
Certified Humane <castrolwilliam@gmail.com>
2022-10-26 11:59:18 -0400

    Update __init__.py

--- Python/Lidar/__init__.py
+++ Python/Lidar/__init__.py
@@ -6,54 +6,89 @@ import time
 
 class Lidar:
     def __init__(self):
-        
+        #Define variables parameters
+        self.port= ""
+        self.laser = ""
+        self.scan = ""
+        self.ret = ""
+        #Declarin Lidar data 
+        self.x =[]
+        self.y =[]
+
+        self.Get_port();
+        self.Parameters();
+        self.Initialize_SDK();
+    
+
+#This function detects for a Lidar connected through usb
+    def Get_port(self):
         ydlidar.os_init();
 
-        ports = ydlidar.lidarPortList()
+        #Get usb port list
+        ports = ydlidar.lidarPortList();
+
         self.port = "/dev/ydlidar";
 
+        #Look for port address
         for key, value in ports.items():
             self.port = value;
-        
+
+
+    #Define All The lidar parameters
+    def Parameters(self):
         self.laser = ydlidar.CYdLidar();
-
-
-        self.parameters();
-
-        self.ret = self.laser.initialize();
-        
-        if self.ret:
-            self.ret = self.laser.turnOn();
-            self.scan = ydlidar.LaserScan();
-
-            while self.ret and ydlidar.os_isOk():
-                r = self.laser.doProcessSimple(self.scan)
-                if r:
-                    
-                    range=[]
-                    angle=[]
-
-                    for point in self.scan.points:
-
-                        angle.append(point.angle);
-                        range.append(point.range); 
-                        
-                    print(angle)
-                    print(range)
-                else:
-                    print("Failed to get Lidar data")
-
-    #This are the paramerts for my lidar wich is the YDLIDAR X2L 
-
-    def parameters(self):
+        #Declare the usb address to the SDK
         self.laser.setlidaropt(ydlidar.LidarPropSerialPort, self.port);
+        #Declare the usb Baudrate (this is different for every lidar)
         self.laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 115200);
+        #Declare the Type of Lidar (In my case the lidar type is a Triangulation lidar)
         self.laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TRIANGLE);
+        #Declare the Device type for this device
         self.laser.setlidaropt(ydlidar.LidarPropDeviceType, ydlidar.YDLIDAR_TYPE_SERIAL);
+        #Define the lidar Frequency 
         self.laser.setlidaropt(ydlidar.LidarPropScanFrequency, 8);
-        self.laser.setlidaropt(ydlidar.LidarPropSampleRate, 9);
+        #Define the number of samplse per second
+        self.laser.setlidaropt(ydlidar.LidarPropSampleRate, 1);
+        #Define if the lidar have multiple channels for communication
+            #(a Chanel for Upload only)
+            #(a Channel for Downloda only)
+            #My lidar has a single channel so is True 
         self.laser.setlidaropt(ydlidar.LidarPropSingleChannel, True);
 
+#Initialize the SDK for The lidar 
+    def Initialize_SDK(self):
+        self.ret = self.laser.initialize();
+        if self.ret:
+            
+            #Turn lidar Laser on
+            self.ret = self.laser.turnOn();
+            #Get Laser Scan Data Raw
+            self.scan =ydlidar.LaserScan(); 
+            
+
+            #Start extracting data
+            self.Extract_Data();
+            
+        time.sleep(0.0275)
+#Disconnect lidar from program
+        self.laser.disconnecting()
 
 
-Lidar();
+#Extract Data from raw laser data 
+    def Extract_Data(self):
+        #Create while loop to extract Lidar data
+            while self.ret and ydlidar.os_isOk():
+                r = self.laser.doProcessSimple(self.scan);
+                if r:
+                    angle =[]
+                    range =[]
+                    for point in self.scan.points:
+                        angle.append(point.angle)
+                        range.append(point.range)    
+
+                    
+                else:
+                    print("Failed to get Lidar Data")
+            
+            #turn off Laser
+            self.laser.turnOff();
