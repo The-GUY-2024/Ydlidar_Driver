8f09c994a22c7a7369c475b314ec56864e1ee468
Certified Humane <castrolwilliam@gmail.com>
2022-10-27 10:23:21 -0400

    Update setup.py

--- Python/setup.py
+++ Python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='Lidar',
   version='0.1',
-  description= 'this a hander for my ydlidar ',
+  description= 'this a hander for my ydlidar xl lidar ',
   url='#',
   author="Louis W. Castro",
   license='MIT',
