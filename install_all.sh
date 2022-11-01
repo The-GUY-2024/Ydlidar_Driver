39cc2b655ce74d711a79326b1925194760052a51
Certified Humane <castrolwilliam@gmail.com>
2022-11-01 09:16:04 -0400

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+
+
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED_B='\e[1;31m'
+
+sudo apt-get update
+echo -e $TEXT_YELLOW
+echo 'APT update finished.... '
+echo -e $TEXT_RESET
+
+
+sudo apt-get upgrade 
+echo -e $TEXT_YELLOW
+echo 'APT upgrade finished.... '
+echo -e $TEXT_RESET
+
+
+DIR=$(dirname $0)
+
+
+#Install the handler writing by me 
+cd Python
+
+sudo pip3 install .
+
+cd ..
+
 
